dc03afbdce53f1efc4d18ac0bfaa4ba4b7a8213c
Abhishek <abhishek@immersionx.io>
2024-03-13 16:00:39 +0530

    light intensity play

--- Assets/_Scripts/Instantiate512.cs
+++ Assets/_Scripts/Instantiate512.cs
@@ -1,26 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 public class Instantiate512 : MonoBehaviour
 {
 
     public GameObject _sampleCube;
     public AudioPeer AudioPeer;
-    public GameObject[] _sampleCubes = new GameObject[512];
+    public GameObject[] _sampleCubes;
     public float _maxScale = 10f;
+
+
+    //Angles
+    public float fDistance;
+    public float startAngle;
+    public float minAngle;
+    public float maxAngle;
 
     // Start is called before the first frame update
     void Start()
     {
+        _sampleCubes = new GameObject[512];
         for (int i = 0; i < _sampleCubes.Length; i++)
         {
             /*GameObject _instanceSampleCube = Instantiate(_sampleCube, Vector3.forward * 100, Quaternion.Euler(new Vector3(0, -0.703125f * i, 0)), this.transform);*/
             GameObject _instanceSampleCube = Instantiate(_sampleCube, this.transform);
             //_instanceSampleCube.transform.position = this.transform.position;
-           this.transform.rotation = Quaternion.Euler(0f, -0.703125f * i, 0f);
+
+            //yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+            this.transform.rotation = Quaternion.Euler(-0.703125f * i, 0f, 0f);
 
             _instanceSampleCube.transform.position = Vector3.forward * 100;
             _instanceSampleCube.transform.rotation = Quaternion.LookRotation(new Vector3(0f, -0.703125f * (i == 0 ? 1 : i), 0f));
@@ -33,29 +41,31 @@ public class Instantiate512 : MonoBehaviour
         {
             int cubeRange = _sampleCubes.Length / AudioPeer._audioBandBuffer.Length;
             int cubeRangeMin = (cubeRange * i) + i;
-            Color _color = new Color(Random.Range(0.5f, 1f), Random.Range(0.5f, 1f), Random.Range(0.5f, 1f));
+            Color _color = new Color(Random.Range(0, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             for (int j = cubeRangeMin - i; j < cubeRange * (i + 1); j++)
             {
-                _sampleCubes[j].GetComponentInChildren<MeshRenderer>().material.color = _color;
+                Material mymat = _sampleCubes[j].GetComponentInChildren<MeshRenderer>().material;
+                mymat.SetColor("_Color", _color);
+                mymat.SetColor("_EmissionColor", _color * (j == cubeRangeMin - i ? 10f : j == (cubeRange * (i + 1)) - 1 ? 10f : 5f));
             }
         }
-        //CalculateRadial();
     }
 
     void CalculateRadial()
     {
         if (transform.childCount == 0)
             return;
-        float fOffsetAngle = ((360f - 0f)) / (transform.childCount - 1);
+        float fOffsetAngle = ((maxAngle - minAngle)) / transform.childCount;
 
-        float fAngle = 0f;
+        float fAngle = startAngle;
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform child = transform.GetChild(i);
             if (child != null)
             {
                 Vector3 vPos = new Vector3(Mathf.Cos(fAngle * Mathf.Deg2Rad), Mathf.Sin(fAngle * Mathf.Deg2Rad), 0);
-                child.position = Vector3.forward * 100f;
+                child.position = Vector3.Cross(vPos, Vector3.forward).normalized * fDistance;
+                child.rotation = Quaternion.Euler(Mathf.Asin(vPos.x) * 180f / Mathf.PI, Mathf.Asin(vPos.y) * 180f / Mathf.PI, vPos.z);
                 fAngle += fOffsetAngle;
             }
         }
@@ -85,6 +95,8 @@ public class Instantiate512 : MonoBehaviour
         //        _sampleCubes[j].transform.localScale = new Vector3(1, (AudioPeer._audioBandBuffer[i] * _maxScale) + 2, 1);
         //    }
         //}
+        //CalculateRadial();
+        //if (Input.GetKey(KeyCode.LeftControl))
         for (int i = 0; i < AudioPeer._audioBandBuffer.Length; i++)
         {
             int cubeRange = _sampleCubes.Length / AudioPeer._audioBandBuffer.Length;
